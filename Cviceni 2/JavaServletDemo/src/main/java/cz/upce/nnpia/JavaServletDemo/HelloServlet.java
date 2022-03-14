6dceda631884139491b23d5d804845c0b5f7833f
NaraaB <sanchir65@gmail.com>
2022-03-14 23:22:51 +0100

    Task 2 completed

--- Cviceni 2/JavaServletDemo/src/main/java/cz/upce/nnpia/JavaServletDemo/HelloServlet.java
+++ Cviceni 2/JavaServletDemo/src/main/java/cz/upce/nnpia/JavaServletDemo/HelloServlet.java
@@ -1,22 +1,29 @@
 package cz.upce.nnpia.JavaServletDemo;
 
 import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello Servlet!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("The Post request has been made to /hello-servlet");
+    }
 
-        // Hello
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("The Get request has been made to /hello-servlet");
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
